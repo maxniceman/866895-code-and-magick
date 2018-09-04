7bfa886cfc9f77f2b00b705169286f62a1dc87b5
maxniceman <maximum666@ukr.net>
2018-09-04 19:44:57 +0200

    added stat.js

--- js/stat.js
+++ js/stat.js
@@ -5,9 +5,8 @@ var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var GAP = 50;
-// var TEXT_WIDTH = 50;
 var BAR_WIDTH = 40;
-var barHeight = 150;
+var BAR_HEIGHT = 150;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -31,20 +30,24 @@ window.renderStatistics = function (ctx, players, times) {
   // welcome text
   ctx.font = '16px PT Mono';
   ctx.fillStyle = '#000';
-  ctx.strokeText('Ура вы победили!', CLOUD_X + GAP, GAP - CLOUD_Y);
-  ctx.fillText('Список результатов:', CLOUD_X + GAP, GAP + CLOUD_Y * 1.5);
+  ctx.fillText('Ура вы победили!', CLOUD_X + GAP, CLOUD_Y * 3);
+  ctx.fillText('Список результатов:', CLOUD_X + GAP, GAP);
 
   // max time
   var maxTime = getMaxElement(times);
   // build chart
   for (var i = 0; i < players.length; i++) {
-    ctx.fillStyle = '#000';
-    ctx.fillText(players[i], CLOUD_X + GAP + (GAP + BAR_WIDTH) * i, CLOUD_HEIGHT - CLOUD_Y);
+    // bars
     if (players[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
       ctx.fillStyle = 'rgba(0,0,139,' + (Math.random() + 0.1) + ')';
     }
-    ctx.fillRect(CLOUD_X + GAP + (GAP + BAR_WIDTH) * i, CLOUD_HEIGHT - GAP / 1.5 - (barHeight * times[i]) / maxTime, BAR_WIDTH, (barHeight * times[i]) / maxTime);
+    ctx.fillRect(CLOUD_X + GAP + (GAP + BAR_WIDTH) * i, CLOUD_HEIGHT - GAP / 2 - (BAR_HEIGHT * times[i]) / maxTime, BAR_WIDTH, (BAR_HEIGHT * times[i]) / maxTime);
+    // names
+    ctx.fillStyle = '#000';
+    ctx.fillText(players[i], CLOUD_X + GAP + (GAP + BAR_WIDTH) * i, CLOUD_HEIGHT - CLOUD_Y / 2);
+    // times
+    ctx.fillText(Math.round(times[i]), CLOUD_X + GAP + (GAP + BAR_WIDTH) * i, CLOUD_HEIGHT - GAP + CLOUD_Y - (BAR_HEIGHT * times[i]) / maxTime);
   }
 };
