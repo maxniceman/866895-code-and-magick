88b0190ec224aaca2d665da7e97db57b3033da7f
Max Nayman <maxim.nayman@dataart.com>
2018-09-04 16:51:12 +0200

    add stat.js draft v2

--- js/stat.js
+++ js/stat.js
@@ -4,7 +4,6 @@ var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
-var SHADOW_GAP = 10;
 var GAP = 50;
 // var TEXT_WIDTH = 50;
 var BAR_WIDTH = 40;
@@ -13,7 +12,7 @@ var barHeight = 150;
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
-}
+};
 
 var getMaxElement = function (arr) {
   var maxElement = arr[0];
@@ -27,25 +26,25 @@ var getMaxElement = function (arr) {
 
 window.renderStatistics = function (ctx, players, times) {
   // cloud box
-  renderCloud(ctx, CLOUD_X + SHADOW_GAP, CLOUD_Y + SHADOW_GAP, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X + CLOUD_Y, CLOUD_Y + CLOUD_Y, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
   // welcome text
   ctx.font = '16px PT Mono';
   ctx.fillStyle = '#000';
-  ctx.strokeText('Ура вы победили!', CLOUD_X + GAP, 50);
-  ctx.fillText('Список результатов:', CLOUD_X + GAP, 75);
+  ctx.strokeText('Ура вы победили!', CLOUD_X + GAP, GAP - CLOUD_Y);
+  ctx.fillText('Список результатов:', CLOUD_X + GAP, GAP + CLOUD_Y * 1.5);
 
   // max time
   var maxTime = getMaxElement(times);
   // build chart
   for (var i = 0; i < players.length; i++) {
     ctx.fillStyle = '#000';
-    ctx.fillText(players[i], CLOUD_X + GAP + (GAP + BAR_WIDTH) * i, CLOUD_HEIGHT - CLOUD_Y * 2);
+    ctx.fillText(players[i], CLOUD_X + GAP + (GAP + BAR_WIDTH) * i, CLOUD_HEIGHT - CLOUD_Y);
     if (players[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
-      ctx.fillStyle = 'rgba(0,0,255,' + Math.random() + ')';
+      ctx.fillStyle = 'rgba(0,0,139,' + (Math.random() + 0.1) + ')';
     }
-    ctx.fillRect(CLOUD_X + GAP + (GAP + BAR_WIDTH) * i, CLOUD_HEIGHT - GAP - barHeight, BAR_WIDTH, (barHeight * times[i]) / maxTime);
+    ctx.fillRect(CLOUD_X + GAP + (GAP + BAR_WIDTH) * i, CLOUD_HEIGHT - GAP / 1.5 - (barHeight * times[i]) / maxTime, BAR_WIDTH, (barHeight * times[i]) / maxTime);
   }
 };
